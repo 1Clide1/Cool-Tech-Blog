2627cd115da2d78a4acd35f36ae14dbcd2049670
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-01-25 15:47:21 -0500

    fixed the seed placement that way it works properly and also added console logs to show when it is done seeding

--- seeds/index.js
+++ seeds/index.js
@@ -5,15 +5,18 @@ const seedBlogPost= require('./blogPostData');
 const seedUser= require('./userData');
 
 // this is how the seeds are run
+// make sure the seeds are in order that way it is able to be seeded properly
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
+  await seedUser();
+  console.log('\n----- USER SEEDED -----\n');
+
   await seedBlogPost();
+  console.log('\n----- BLOGPOST SEEDED -----\n');
 
   await seedComment();
-
-  await seedUser();
-
+  console.log('\n----- COMMENTS SEEDED -----\n');
   process.exit(0);
 };
 

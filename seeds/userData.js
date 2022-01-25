65c80cfa71a92baa942d3936e4ebcda2a76b27aa
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-01-25 15:47:42 -0500

    finished the seeds for the users

--- seeds/userData.js
+++ seeds/userData.js
@@ -2,16 +2,24 @@ const { User } = require('../models');
 
 const userData = [
   {
-    
+    username: "Brandon",
+    email: "brandon@gmail.com",
+    password: "password"
   },
   {
-    
+    username: "Clide",
+    email: "clide@gmail.com",
+    password: "password"
   },
   {
-   
+    username: "Randy",
+    email: "randy@gmail.com",
+    password: "password"
   },
   {
-    
+    username: "John",
+    email: "jojo@gmail.com",
+    password: "password"
   }
 ];
 

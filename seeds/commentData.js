b353fd246450260d0630e4177e1d197fa62f5f85
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-01-25 15:46:27 -0500

    finished the seeds for the comments

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -2,16 +2,24 @@ const { Comment } = require('../models');
 
 const commentData = [
   {
-    
+    user_id: 1,
+    blogpost_id: 1,
+    comment_text: "feel free to comment here :)"
   },
   {
-    
+    user_id: 3,
+    blogpost_id: 2,
+    comment_text: "Nice!!"
   },
   {
-   
+    user_id: 2,
+    blogpost_id: 3,
+    comment_text: "dang that's pretty cool"
   },
   {
-    
+    user_id: 1,
+    blogpost_id: 4,
+    comment_text: "they are dope, go for it!"
   }
 ];
 

1103f27fc3779c9adb0b4c4eb2d999becce4f0df
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-01-26 16:24:52 -0500

    formatted the footer handlebars and also finished the api index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 // import router
-const router = require('express').Router();
+const router = require("express").Router();
 // import the routes
-const userRoutes = require('./user-routes');
+const userRoutes = require("./user-routes");
+const commentRoutes = require("./comments-routes");
+const blogPostRoutes = require("./blogpost-routes");
 // these are where the api routes go
-router.use('/users', userRoutes);
+router.use("/users", userRoutes);
+router.use("/blogpost", blogPostRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
